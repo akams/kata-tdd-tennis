d3b659e69d849103347424c00f7b79d983b05bf4
akams <e.ndjawe@gmail.com>
2020-11-18 17:15:06 +0100

    refactor: pour éviter de déclarer dans chaque test

--- user.test.js
+++ user.test.js
@@ -1,12 +1,17 @@
 const User = require('./user'); 
 
 describe('User', () => {
+  let p1;
+  beforeEach(() => {
+    p1 = new User('Player one', 0);
+  });
+  afterEach(() => {
+    p1 = undefined;
+  });
   it('should initialize user', () => {
-    const p1 = new User('Player one', 0);
     expect(p1.score).toBe(0);
   });
   it('should add point to user and must be equal to 15', () => {
-    const p1 = new User('Player one', 0);
     p1.addPoint();
     expect(p1.score).toBe(15);
   });
