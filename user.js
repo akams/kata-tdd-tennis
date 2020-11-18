72c7ff2fb4202d2ceb0708aa01dca694e775de41
akams <e.ndjawe@gmail.com>
2020-11-18 17:34:44 +0100

    tdd: success for notice user is winner count equal 1

--- user.js
+++ user.js
@@ -1,6 +1,7 @@
 class User {
   #name = 0;
   #score;
+  #winnerCount = 0;
   constructor(name, score) {
     this.#name = name;
     this.#score = score;
@@ -12,11 +13,16 @@ class User {
     return this.#score;
   }
 
+  get winnerCount() {
+    return this.#winnerCount;
+  }
+
   addPoint() {
     if (this.#score === 30) {
       this.#score += 10;
     } else if (this.#score === 40) {
       this.#score = 0;
+      this.#winnerCount += 1;
     } else {
       this.#score += 15;
     }
